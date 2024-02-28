95e8b2e12eee56d13c181533f489b370a18308f3
Cesar Santos <csar.santos18@gmail.com>
2024-02-27 20:10:35 -0500

    commit test

--- AulaUnimeta/App.js
+++ AulaUnimeta/App.js
@@ -13,9 +13,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eaeaea',
     alignItems: 'center',
     justifyContent: 'center',
+
 
   },
 })
